905c5aaae5c3705aa8bd263033f87a495f27f7f3
hashim-balouch <hashimcpro@gmail.com>
2024-11-09 17:00:57 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // base: "/react-based-POS-system", //doesn't works when deployed!
+  base: "/react-based-POS-system/", //doesn't works when deployed!
   plugins: [react()], // Replace <your-repo-name> with your actual repository name
 });
